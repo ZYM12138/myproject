72d25b1adbc7006261599404c7b68db92ae80a15
Vincent <769351040@qq.com>
2020-07-23 19:52:34 +0800

    first version

--- learnJava/src/ObjectOriented/Student.java
+++ learnJava/src/ObjectOriented/Student.java
@@ -1,24 +1,8 @@
 package ObjectOriented;
 
 public class Student {
-    private String name;
-    private int age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
+    String name;
+    int age;
 
     public void eat(){
         System.out.println("吃");
