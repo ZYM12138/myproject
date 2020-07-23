a53b62403aa274f13ab9ad524a3ab86deaa1a55b
Vincent <769351040@qq.com>
2020-07-23 19:55:43 +0800

    first version

--- learnJava/src/ObjectOriented/Demo02Student.java
+++ learnJava/src/ObjectOriented/Demo02Student.java
@@ -3,7 +3,7 @@ package ObjectOriented;
 public class Demo02Student {
     public static void main(String[] args) {
         Student stu=new Student();
-        System.out.println(stu.getName());
-        System.out.println(stu.getAge());
+        System.out.println(stu.name);
+        System.out.println(stu.age);
     }
 }
