8659257d8470a51b020633a43c65838e0c694d47
Vincent <769351040@qq.com>
2020-07-28 01:15:28 +0800

    first version

---
+++ learnJava/src/ObjectOriented/day2/Test.java
@@ -0,0 +1,14 @@
+package ObjectOriented.day2;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new Student("张三",18).getName());
+        System.out.println(getStudent());;
+    }
+    public static String getStudent(){
+        return new Student("李四",20).getName();
+    }
+
+}
