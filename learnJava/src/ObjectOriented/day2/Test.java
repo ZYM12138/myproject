1b7902d94c291da19176796d5402d63ccc92707f
Vincent <769351040@qq.com>
2020-07-28 01:16:50 +0800

    first version

--- learnJava/src/ObjectOriented/day2/Test.java
+++ learnJava/src/ObjectOriented/day2/Test.java
@@ -1,7 +1,5 @@
 package ObjectOriented.day2;
 
-import java.util.Scanner;
-
 public class Test {
     public static void main(String[] args) {
         System.out.println(new Student("张三",18).getName());
