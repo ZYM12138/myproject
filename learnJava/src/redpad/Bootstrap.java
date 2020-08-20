4a478164ce46dcee2918b1c83564a05d532113fe
Vincent <769351040@qq.com>
2020-08-20 15:25:50 +0800

    first version

---
+++ learnJava/src/redpad/Bootstrap.java
@@ -0,0 +1,8 @@
+package redpad;
+
+public class Bootstrap {
+    public static void main(String[] args) {
+        MyRed red = new MyRed("红包");
+
+    }
+}
---
+++ learnJava/src/redpad/OpenMode.java
@@ -0,0 +1,8 @@
+package redpad;
+
+import java.util.ArrayList;
+
+public interface OpenMode {
+
+    ArrayList<Integer> divide(int totalMoney,int totalCount);
+}
